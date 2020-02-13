dd1ab3d3710f3ab11dc426157e3cc8e7f3a8a238
Peter Wildberger <pete.wildberger@gmail.com>
2020-02-12 22:10:42 -0600

    improve documentation

--- readme.md
+++ readme.md
@@ -6,6 +6,26 @@
 
 The code in this repo is written in typescript. Typescript will install as part of the dev dependencies. You can also install typescript globally on your machine by running `sudo npm install -g typescript`. To build and start the project run `npm start` or `yarn start`.
 
+## How it Works
+
+The App requires two arguments: a url and a config object. The url is a string http(s) website address. The config object consists of a parent key which has a css selector string values. The children key is and object with key value pairs of string keys and css selector string values.
+
+```
+const URL: string = 'https://www.thecedar.org/listing';
+const CONFIG: ConfigModel = {
+    parent: 'a container element css selector',
+    children: {
+        key: 'any child element css selector'
+    }
+};
+```
+
+To start the app just make a new instance by injecting the url and config props.
+
+```
+new App(URL, CONFIG);
+```
+
 ## Docs
 
 [Cherrio Docs](https://cheerio.js.org/)
--- src/App.class.ts
+++ src/App.class.ts
@@ -11,20 +11,21 @@ export interface ConfigModel {
 export class App {
     private config: ConfigModel;
     private url: string;
+
     constructor(url: string, config: ConfigModel) {
         console.log('App started');
         this.url = url;
         this.config = config;
-        this.run();
+        this.run(); // run app
     }
-
+    // main app method
     run = async (): Promise<void> => {
         try {
-            const { data } = await axios.get(this.url);
-            const $ = cheerio.load(data);
-            const els = this.selectElements($, this.config.parent);
-            const shows = this.buildResult($, els, this.config.children);
-            console.log(shows);
+            const { data } = await axios.get(this.url); // make http request to get site data
+            const $ = cheerio.load(data); // load Cheerio
+            const els = this.selectElements($, this.config.parent); // select parent elements
+            const shows = this.buildResult($, els, this.config.children); // build result object array
+            console.log(shows); // print result
         } catch (error) {
             console.log(error);
         }
@@ -33,6 +34,7 @@ export class App {
     selectElements($: CheerioStatic, selector: string): CheerioElement[] {
         const elements: CheerioElement[] = [];
         $(selector).each((i, elem) => {
+            // select all elements based of parent config and push into an array
             elements[i] = elem;
         });
         return elements;
@@ -40,6 +42,7 @@ export class App {
 
     buildResult($: CheerioStatic, elements: CheerioElement[], config: { [key: string]: string }): Array<{ [key: string]: string }> {
         return elements.map(el => {
+            // build result object for each element
             const result = {};
             Object.entries(config).forEach(([key, value]) => {
                 result[key] = $(el)
